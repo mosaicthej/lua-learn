f45da0c24b6376aeedca7c1d0bad71e1d9f31443
mosaicthej <mij623@usask.ca>
2024-05-30 23:48:43 -0600

    some lexical styles and conventions

---
+++ ch01/05-comments.lua
@@ -0,0 +1,31 @@
+--[[ RESERVED WORDS:
+--
+  and break do else elseif
+ end false for function goto
+ if in local nil not
+ or repeat return then true
+ until while
+--]]
+
+-- single line comment
+
+--[[ multi
+line 
+  long
+comment
+]]
+
+print(10) -- this is a code that does something
+
+--[[
+  print(10)
+--]]
+--nowthe code is commented out
+--
+
+---[[
+  print(10)
+--]] 
+-- now the code came back to life easily (add - on line 1)
+-- this creates less/no undesired side effects with comments.
+
---
+++ ch01/06-semi.lua
@@ -0,0 +1,19 @@
+-- lua needs no separator between statments.
+-- SEMI is optional.
+-- line break does not do anything.
+--
+-- These four chuncksare all valid and equivalent
+--
+-- 1.
+A = 1
+B = A*2
+
+-- 2
+A = 1;
+B = A*2;
+
+-- 3
+A = 1; B = A*2
+
+-- 4 (ugly)
+A = 1 B = A * 2
